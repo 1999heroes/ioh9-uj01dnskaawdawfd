51322fb1ff307e5cc87f869c88db9ceaeaa38331
1999heroes <143978318+1999heroes@users.noreply.github.com>
2023-09-17 16:32:09 -0700

    Update Watermark.lua

--- Watermark.lua
+++ Watermark.lua
@@ -1,58 +1,38 @@
--- Gui to Lua
--- Version: 3.2
-
--- Instances:
+-- masta mark
 
 local ScreenGui = Instance.new("ScreenGui")
-local TextLabel = Instance.new("TextLabel")
-local UICorner = Instance.new("UICorner")
-local UIGradient = Instance.new("UIGradient")
-local mastaskid = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
 local ImageLabel = Instance.new("ImageLabel")
-
---Properties:
 
 ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
-TextLabel.Parent = ScreenGui
-TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
-TextLabel.BorderSizePixel = 10
-TextLabel.Position = UDim2.new(0.0683229044, 0, 0.326017827, 0)
-TextLabel.Size = UDim2.new(0, 156, 0, 45)
-TextLabel.Font = Enum.Font.LuckiestGuy
-TextLabel.Text = "FPS"
-TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
-TextLabel.TextSize = 30.000
+Frame.Parent = ScreenGui
+Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame.BackgroundTransparency = 1.000
+Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Frame.BorderSizePixel = 0
+Frame.Size = UDim2.new(0, 100, 0, 100)
 
-UICorner.CornerRadius = UDim.new(0, 100)
-UICorner.Parent = TextLabel
-
-UIGradient.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(245, 134, 255)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(255, 255, 255))}
-UIGradient.Parent = TextLabel
-
-mastaskid.Name = "mastaskid"
-mastaskid.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-mastaskid.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-
-ImageLabel.Parent = mastaskid
+ImageLabel.Parent = Frame
 ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 ImageLabel.BackgroundTransparency = 1.000
 ImageLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
 ImageLabel.BorderSizePixel = 0
-ImageLabel.Position = UDim2.new(-0.0463362075, 0, 0, 0)
-ImageLabel.Size = UDim2.new(0, 390, 0, 227)
-ImageLabel.Image = "rbxassetid://14703329271"
+ImageLabel.Position = UDim2.new(-0.670000017, 0, 5.94000006, 0)
+ImageLabel.Size = UDim2.new(0, 552, 0, 262)
+ImageLabel.Image = "rbxassetid://14811035027"
 
--- Scripts:
+local function blackballs()
+	local script = Instance.new('LocalScript', ImageLabel)
 
-local function ZNVU_fake_script() -- TextLabel.FPS 
-	local script = Instance.new('LocalScript', TextLabel)
-
+	local image = script.Parent
+	
 	while wait() do
-		local amount = math.floor(workspace:GetRealPhysicsFPS())
-		script.Parent.Text = "FPS:"..amount
+		image.Image = "rbxassetid://14811035027"
+		wait(2)
+		image.Image = "rbxassetid://14811060476"
+		wait(2)
 	end
 end
-coroutine.wrap(ZNVU_fake_script)()
+coroutine.wrap(blackballs)()
